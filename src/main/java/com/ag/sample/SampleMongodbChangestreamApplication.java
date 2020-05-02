9474ed068bb6be76e22b88b9f3d67c5108cbca70
adityagoel <adityadecember25@gmail.com>
2020-05-02 12:13:28 -0500

    initial commit

---
+++ src/main/java/com/ag/sample/SampleMongodbChangestreamApplication.java
@@ -0,0 +1,13 @@
+package com.ag.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMongodbChangestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMongodbChangestreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag/sample/SampleMongodbChangestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMongodbChangestreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
